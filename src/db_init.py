9710614ea30ff0989d2806aa68a39b470081c1cc
giorgio-mariani <giorgiomariani94@gmail.com>
2018-11-25 17:33:04 +0100

    update

--- src/db_init.py
+++ src/db_init.py
@@ -2,49 +2,59 @@ import psycopg2 as ps
 
 import db_utils
 
-conn = ps.connect(
-    dbname="test_db",
-    user="postgres",
-    password="postgres")
-cur = conn.cursor()
+def db_init(database_url):
+    conn = ps.connect(database_url)
+    cur = conn.cursor()
 
-#-------------------------------------------------------------------------------
-# Create necessary tables
+    #---------------------------------------------------------------------------
+    # Create necessary tables
 
-cur.execute(
-    '''CREATE TABLE Users (
-        user_id serial PRIMARY KEY,
-        username VARCHAR(255) NOT NULL UNIQUE,
-        password VARCHAR(255) NOT NULL);''')
+    cur.execute(
+        '''CREATE TABLE Users (
+            user_id serial PRIMARY KEY,
+            username VARCHAR(255) NOT NULL UNIQUE,
+            password VARCHAR(255) NOT NULL);''')
 
-cur.execute(
-    '''CREATE TABLE Messages (
-        message_id serial PRIMARY KEY,
-        sender_id int NOT NULL REFERENCES Users,
-        message_text text NOT NULL,
-        timestamp timestamp with time zone NOT NULL);''')
+    cur.execute(
+        '''CREATE TABLE Messages (
+            message_id serial PRIMARY KEY,
+            sender_id int NOT NULL REFERENCES Users,
+            message_text text NOT NULL,
+            timestamp timestamp with time zone NOT NULL);''')
 
-cur.execute(
-    '''CREATE TABLE Receivers (
-        message_id int NOT NULL REFERENCES Messages ON DELETE CASCADE,
-        receiver_id int NOT NULL REFERENCES Users,
-        message_read bool NOT NULL DEFAULT FALSE);''')
-conn.commit()
+    cur.execute(
+        '''CREATE TABLE Receivers (
+            message_id int NOT NULL REFERENCES Messages ON DELETE CASCADE,
+            receiver_id int NOT NULL REFERENCES Users,
+            message_read bool NOT NULL DEFAULT FALSE);''')
+    conn.commit()
 
-#-------------------------------------------------------------------------------
-# Insert users into the database
+    #---------------------------------------------------------------------------
+    # Populate user table
 
-users = [("TamasRadvany", 'password'),
-         ("GiorgioMariani", 'password'),
-         ("DonaldKnuth","1 2 3 4 5 7"),
-         ("xX_Memelord99_Xx","PaSsWOrD"),
-         ("bob", "bob")]
-for u,p in users: db_utils.add_user(cur,u,p)
-conn.commit()
+    users = [("TamasRadvany", 'password'),
+            ("GiorgioMariani", 'password'),
+            ("DonaldKnuth","1 2 3 4 5 7"),
+            ("xX_Memelord99_Xx","PaSsWOrD"),
+            ("bob", "bob")]
+    for u,p in users: db_utils.add_user(cur,u,p)
+    conn.commit()
 
-#-------------------------------------------------------------------------------
+    #---------------------------------------------------------------------------
 
-cur.close()
-conn.close()
+    cur.close()
+    conn.close()
 
-print("Database initialized!")
+    print("Database initialized!")
+
+
+#===============================================================================
+if __name__ == '__main__':
+    import sys
+
+    if len(sys.argv) == 1:
+        print("usage: python db_init.py <database_url>")
+    else:
+        database_url = sys.argv[1]
+        db_init(database_url)
+
